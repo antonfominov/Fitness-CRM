3f4493afcf5ca381da3b90c99bd0cdb3ab32f4f8
Anton Fominov <anton-fominov@bk.ru>
2021-07-10 19:53:43 +0300

    Рефакторинг навигации для react-router

--- src/main/resources/static/src/components/Navigation/Navigation.jsx
+++ src/main/resources/static/src/components/Navigation/Navigation.jsx
@@ -1,41 +1,51 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
 
 import CityGrid from '../CityGrid/CityGrid';
+import ClubGrid from '../ClubGrid/ClubGrid';
 
 import './navigation.css';
 const { Header, Content, Footer, Sider } = Layout;
 
 const Navigation = () => {
   return (
-    <Layout>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}>
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-          <Menu.Item key="1">Города</Menu.Item>
-          <Menu.Item key="2">Клубы</Menu.Item>
-          <Menu.Item key="3">Тренировки</Menu.Item>
-          <Menu.Item key="4">nav 4</Menu.Item>
-        </Menu>
-      </Sider>
+    <BrowserRouter>
       <Layout>
-        <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-            <CityGrid />
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2021 Anton Fominov</Footer>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <NavLink to="/">Города</NavLink>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <NavLink to="/clubs">Клубы</NavLink>
+            </Menu.Item>
+            <Menu.Item key="3">Тренировки</Menu.Item>
+            <Menu.Item key="4">Пользователи</Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 595 }}>
+              <Route exact path="/" component={CityGrid} />
+              <Route exact path="/clubs" component={ClubGrid} />
+              {/* <CityGrid /> */}
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Anton Fominov</Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </BrowserRouter>
   );
 };
 
