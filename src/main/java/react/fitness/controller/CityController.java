2e50ff856058984c47e57a3504f9f50135474b9f
Anton Fominov <anton-fominov@bk.ru>
2021-07-11 19:48:39 +0300

    Добавление контроллера удаления города

--- src/main/java/react/fitness/controller/CityController.java
+++ src/main/java/react/fitness/controller/CityController.java
@@ -1,11 +1,14 @@
 package react.fitness.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import react.fitness.entity.City;
@@ -17,9 +20,22 @@ public class CityController {
 @Autowired
 private CityRepository cityRepository;	
 
-	@CrossOrigin(origins = "http://localhost:8080")
-	@GetMapping ("/api/cities")
-		public Iterable<City> getAll(){
-			return cityRepository.findAll();
-		}
+@CrossOrigin(origins = "http://localhost:8080")
+@GetMapping ("/api/cities")
+public Iterable<City> getAll(){
+	return cityRepository.findAll();
+}
+	
+@CrossOrigin(origins = "http://localhost:8080")
+@DeleteMapping ("/api/cities")
+public Boolean delete(@RequestParam Integer id){
+	System.out.println("ID" + id);
+	try {
+		cityRepository.deleteById(id);
+		return true;
+	} catch (NullPointerException e) {
+		return false;
+	}
+}
+
 }
