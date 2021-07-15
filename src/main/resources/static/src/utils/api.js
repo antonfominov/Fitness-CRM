0a8bbc8e60da7f78afd901a5dfe4656ec15481f9
Anton Fominov <anton-fominov@bk.ru>
2021-07-16 00:32:13 +0300

    Исправления апи

--- src/main/resources/static/src/utils/api.js
+++ src/main/resources/static/src/utils/api.js
@@ -1,15 +1,39 @@
 import * as axios from 'axios';
 
-const URL = 'http://localhost:8090/api';
+axios.defaults.baseURL = 'http://localhost:8090/api';
+
+//const URL = 'http://localhost:8090/api';
 
 export const getCities = () => {
-  return axios.get(URL + `/cities`).then((response) => {
-    return response.data;
-  });
+  return axios
+    .get(`/cities`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
 
 export const deleteCity = (props) => {
-  return axios.delete(URL + `/cities`, { params: { id: props.id } }).then((response) => {
-    return response.data;
-  });
+  return axios
+    .delete(`/cities`, { params: { id: props.id } })
+    .then((response) => {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+export const createCity = (props) => {
+  return axios
+    .post(`/cities`, { ...props })
+    .then((response) => {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return false;
+    });
 };
