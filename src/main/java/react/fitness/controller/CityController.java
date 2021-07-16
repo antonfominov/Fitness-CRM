f252fd34509125040dc536438159f01d5db01340
Anton Fominov <anton-fominov@bk.ru>
2021-07-16 23:24:12 +0300

    Рефакторинг контроллера городов

--- src/main/java/react/fitness/controller/CityController.java
+++ src/main/java/react/fitness/controller/CityController.java
@@ -4,32 +4,41 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import react.fitness.entity.City;
 import react.fitness.repository.CityRepository;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
+@RequestMapping("/api/cities")
 public class CityController {
 	
 @Autowired
 private CityRepository cityRepository;	
 
-@CrossOrigin(origins = "http://localhost:8080")
-@GetMapping ("/api/cities")
+@GetMapping
 public Iterable<City> getAll(){
 	return cityRepository.findAll();
 }
+
+@PostMapping
+public ResponseEntity<Object> createCity(@RequestBody City city){
+    City savedCity = cityRepository.save(city);
+    return ResponseEntity.ok(savedCity);
+}
 	
-@CrossOrigin(origins = "http://localhost:8080")
-@DeleteMapping ("/api/cities")
+@DeleteMapping
 public Boolean delete(@RequestParam Integer id){
-	System.out.println("ID" + id);
 	try {
 		cityRepository.deleteById(id);
 		return true;
