889637e7b39dcf96f33d086bf4f963f475d88855
Anton Fominov <anton-fominov@bk.ru>
2021-07-17 17:12:22 +0300

    Метод обновления информации о городе

--- src/main/java/react/fitness/controller/CityController.java
+++ src/main/java/react/fitness/controller/CityController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,6 +36,15 @@ public Iterable<City> getAll(){
 public ResponseEntity<Object> createCity(@RequestBody City city){
     City savedCity = cityRepository.save(city);
     return ResponseEntity.ok(savedCity);
+}
+
+@PutMapping
+public ResponseEntity<Object> updateCity(@RequestBody City city) {
+    City currentCity = cityRepository.findById(city.getId()).orElseThrow(RuntimeException::new);
+    currentCity.setName(city.getName());
+    currentCity = cityRepository.save(city);
+
+    return ResponseEntity.ok(currentCity);
 }
 	
 @DeleteMapping
