57b5f3b652304caafba60a7a00b94f33d34b4343
antonfominov <61196201+antonfominov@users.noreply.github.com>
2021-11-30 15:33:29 +0300

    Переход на JpaRepository

--- src/main/java/react/fitness/repository/CityRepository.java
+++ src/main/java/react/fitness/repository/CityRepository.java
@@ -1,11 +1,12 @@
 package react.fitness.repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import react.fitness.entity.City;
 
 @Repository
-public interface CityRepository extends CrudRepository<City, Integer> {
+public interface CityRepository extends JpaRepository<City, Long> {
 }
