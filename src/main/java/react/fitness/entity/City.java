489633b73fb480009315519f1e2f6426f15ee394
antonfominov <61196201+antonfominov@users.noreply.github.com>
2021-11-30 15:34:23 +0300

    Избавляемся от связей @ManyTo

--- src/main/java/react/fitness/entity/City.java
+++ src/main/java/react/fitness/entity/City.java
@@ -10,23 +10,20 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "cities")
 public class City {
-	public City() {
-	}
-	
+
 	@Id
 	@Column(name = "id")
-
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
 	@Column(name = "name")
 	private String name;
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
