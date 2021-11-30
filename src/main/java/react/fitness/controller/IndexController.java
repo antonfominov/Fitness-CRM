66dc2bcd0bd6a2e167c1b5c214cae88ace6d8d29
antonfominov <61196201+antonfominov@users.noreply.github.com>
2021-11-30 15:34:38 +0300

    Фикс crossorigin

--- src/main/java/react/fitness/controller/IndexController.java
+++ src/main/java/react/fitness/controller/IndexController.java
@@ -1,8 +1,10 @@
 package react.fitness.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin
 @Controller
 public class IndexController {
 	@RequestMapping(value = "/")
